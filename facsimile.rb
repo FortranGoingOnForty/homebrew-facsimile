17a70d4eaf9fb4d9de8ecb3649c78902fc1fae0d
espadonne <espadonne@outlook.com>
2025-10-29 15:36:48 +0000

    make the formula

---
+++ facsimile.rb
@@ -0,0 +1,29 @@
+class Facsimile < Formula
+  desc "Terminal text editor written in Fortran with VSCode-style keybindings"
+  homepage "https://github.com/FortranGoingOnForty/facsimile"
+  url "https://github.com/FortranGoingOnForty/facsimile/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "e926e55bd720668d739172f1f440a52fca85085b69a1ab960d6f9ff6a89640d7"
+  license "MIT"
+  head "https://github.com/FortranGoingOnForty/facsimile.git", branch: "trunk"
+
+  depends_on "gcc" # for gfortran
+
+  def install
+    # Build using make
+    system "make"
+
+    # Install binary
+    bin.install "fac"
+
+    # Create symlink for convenience
+    bin.install_symlink "fac" => "facsimile"
+
+    # Install documentation
+    doc.install "README.md"
+  end
+
+  test do
+    # Test that fac can run (basic version check or help)
+    system "#{bin}/fac", "--help" rescue true
+  end
+end
