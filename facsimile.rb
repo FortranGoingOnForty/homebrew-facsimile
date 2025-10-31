7c4ac38c19d88cddf5188ae3663531e3e42e0f2f
espadonne <espadonne@outlook.com>
2025-10-31 19:52:05 +0000

    Update to facsimile 0.5.9
    Extensions to fuss viewer with git operations support

--- facsimile.rb
+++ facsimile.rb
@@ -1,8 +1,8 @@
 class Facsimile < Formula
   desc "Terminal text editor written in Fortran with VSCode-style keybindings"
   homepage "https://github.com/FortranGoingOnForty/facsimile"
-  url "https://github.com/FortranGoingOnForty/facsimile/archive/refs/tags/v0.5.2.tar.gz"
-  sha256 "a0d5ad0f8ad09b8432beab9b90cf96e85f279d0ce7e3e8471b274c939faf372e"
+  url "https://github.com/FortranGoingOnForty/facsimile/archive/refs/tags/v0.5.9.tar.gz"
+  sha256 "dab723505d70f8c66c13c68357080066fdfdddc8c94b8b394da20b2bb64be546"
   license "MIT"
   head "https://github.com/FortranGoingOnForty/facsimile.git", branch: "trunk"
 
