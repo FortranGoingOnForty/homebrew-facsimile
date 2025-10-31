20fde2aeddf53f8e8106e5700ebb7e4d52d5f951
espadonne <espadonne@outlook.com>
2025-10-31 20:28:30 +0000

    Update to facsimile 0.6.0
    Flesh out fuss menu git integrations

--- facsimile.rb
+++ facsimile.rb
@@ -1,8 +1,8 @@
 class Facsimile < Formula
   desc "Terminal text editor written in Fortran with VSCode-style keybindings"
   homepage "https://github.com/FortranGoingOnForty/facsimile"
-  url "https://github.com/FortranGoingOnForty/facsimile/archive/refs/tags/v0.5.9.tar.gz"
-  sha256 "dab723505d70f8c66c13c68357080066fdfdddc8c94b8b394da20b2bb64be546"
+  url "https://github.com/FortranGoingOnForty/facsimile/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "aafe28c745042e6d1d8f6dc7db4ae6795ad18a265a21b52aa5b24b5664207fac"
   license "MIT"
   head "https://github.com/FortranGoingOnForty/facsimile.git", branch: "trunk"
 
